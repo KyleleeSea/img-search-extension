50cee92f67ac446952d992e78bb24f3ba8fe02e2
Kyle Lee <kylelee.sea@gmail.com>
2024-03-04 23:20:25 -0500

    Finds image url

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-async function fetchData() {
-    
-}
+// https://stackoverflow.com/questions/10423670/chrome-extension-that-copies-image-url-on-click
+
+// On right click, get image URL and call API
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "fetchImageURL",
+      title: "Find Similar Clothes",
+      contexts: ["image"]
+    });
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "fetchImageURL" && info.srcUrl) {
+      console.log("Image URL:", info.srcUrl);
+      get_results(info.srcUrl);
+    }
+  });
